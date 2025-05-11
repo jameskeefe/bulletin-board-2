301f2339adff2d9c9c97a3f799145d765462343a
jameskeefe <204756923+jameskeefe@users.noreply.github.com>
2025-05-11 02:22:38 +0000

    added sample data and users

--- app/models/board.rb
+++ app/models/board.rb
@@ -2,14 +2,15 @@
 #
 # Table name: boards
 #
-#  id         :integer          not null, primary key
+#  id         :bigint           not null, primary key
 #  name       :string
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
+#  user_id    :integer
 #
 class Board < ApplicationRecord
   validates(:name, presence: true, uniqueness: true)
 
   has_many(:posts, class_name: "Post", foreign_key: "board_id", primary_key: "id")
-#  belongs_to(:user, class_name: "User", foreign_key: "user_id", primary_key: "id")
+  belongs_to(:user, class_name: "User", foreign_key: "user_id", primary_key: "id")
 end
